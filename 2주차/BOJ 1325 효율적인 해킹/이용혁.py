2e5620faa3244aea8e4e35af57efbee2485373bf
kanto <joshuaisland@gmail.com>
2023-07-21 18:00:05 +0900

    오답(시간초과)

--- 2주차/BOJ 1325 효율적인 해킹/이용혁.py
+++ 2주차/BOJ 1325 효율적인 해킹/이용혁.py
@@ -1,12 +1,26 @@
-
-mydict = [[0] * 3 for _ in range(3)]
-mydict[0][1] = 1
-mydict[0][2] = 1
-visited = [0,0,0]
 def DFS(x):
     ret = 1
-    for loc in range(len(mydict[x])):
-        ret += mydict[x][loc]
+    visited[x] = 1
+    for truster in rel[x]:
+        if not visited[truster]:
+            ret += DFS(truster)
     return ret
 
-print(DFS(0))
+# visited 안걸어 주니깐 무한 recursion이 들어가는 거 같다.
+
+N,M=map(int,input().split())
+rel = [[] for _ in range(N+1)]
+for _ in range(M):
+    a,b = map(int, input().split())
+    rel[b].append(a) # dict 가 시간초과가 있어서 list 로 변경해서 해보자.
+# dic_keys = list(dic.keys()) 이기 없앨 수 있고.
+mx = 0
+arr = [0] * (N + 1)
+for i in range(1,N+1):
+    visited = [0] * (N + 1) # 초기화를 여기서 안해주면 안된다.. for 문 돌 때마다 초기화 되어야 함.
+    arr[i] = DFS(i)
+    mx = max(mx,arr[i])
+
+for i in range(1,N+1):
+    if arr[i] == mx:
+        print(i, end=" ")
