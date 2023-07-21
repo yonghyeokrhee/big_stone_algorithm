89624cc1af6554e6e999006524ddcac0259aa3fd
kanto <joshuaisland@gmail.com>
2023-07-21 13:31:54 +0900

    정답 기록

--- 2주차/BOJ 2910 빈도 정렬/이용혁.py
+++ 2주차/BOJ 2910 빈도 정렬/이용혁.py
@@ -1,12 +1,11 @@
 import collections
 
-N, C = 5, 2
-arr = [1,1,1,2,2,2]
-
+N, C = map(int, input().split())
+arr = list(map(int, input().split()))
 
 def freq(param):
     cnt = collections.Counter(arr)
     cnt_k_ord = [tp[0] for tp in cnt.most_common()]
     return cnt_k_ord.index(param)
-
-print(sorted(arr, key = freq))
+answer= sorted(arr, key = freq)
+print(" ".join(map(str,answer)))
