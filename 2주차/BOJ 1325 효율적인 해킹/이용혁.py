8461a5b9b6421bea5fb8864c83ae3bf6f23dd991
kanto <joshuaisland@gmail.com>
2023-07-21 19:21:42 +0900

    정답 코드

--- 2주차/BOJ 1325 효율적인 해킹/이용혁.py
+++ 2주차/BOJ 1325 효율적인 해킹/이용혁.py
@@ -1,26 +1,40 @@
-def DFS(x):
-    ret = 1
-    visited[x] = 1
-    for truster in rel[x]:
-        if not visited[truster]:
-            ret += DFS(truster)
-    return ret
+import sys
+sys.setrecursionlimit(10000)
 
-# visited 안걸어 주니깐 무한 recursion이 들어가는 거 같다.
+# def DFS(x):
+#     ret = 1
+#     visited[x] = 1
+#     for truster in rel[x]:
+#         if not visited[truster]:
+#             ret += DFS(truster)
+#     return ret
 
-N,M=map(int,input().split())
-rel = [[] for _ in range(N+1)]
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+rel = [[] for _ in range(N + 1)]
 for _ in range(M):
-    a,b = map(int, input().split())
-    rel[b].append(a) # dict 가 시간초과가 있어서 list 로 변경해서 해보자.
-# dic_keys = list(dic.keys()) 이기 없앨 수 있고.
-mx = 0
-arr = [0] * (N + 1)
-for i in range(1,N+1):
-    visited = [0] * (N + 1) # 초기화를 여기서 안해주면 안된다.. for 문 돌 때마다 초기화 되어야 함.
-    arr[i] = DFS(i)
-    mx = max(mx,arr[i])
+    a, b = map(int, input().split())
+    rel[b].append(a)
 
-for i in range(1,N+1):
+mx = 0
+stack = []
+arr = [0] * (N + 1)
+for i in range(1, N + 1):
+    visited = [0] * (N + 1)  # 초기화를 여기서 안해주면 안된다.. for 문 돌 때마다 초기화 되어야 함.
+    stack.append(i)
+    visited[i] = 1
+    while stack:
+        val = stack.pop()
+        arr[i] +=1
+        #leng = len(rel[i])
+        for j in range(len(rel[val])):
+            if len(rel[val]) > 0 and not visited[rel[val][j]]:
+                stack.append(rel[val][j])
+                visited[rel[val][j]] = True
+
+mx = max(arr)
+print(arr)
+for i in range(1, N + 1):
     if arr[i] == mx:
         print(i, end=" ")
