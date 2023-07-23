4bb478c56b6466ae69da524aa6b02eade64a57d0
kanto <joshuaisland@gmail.com>
2023-07-23 19:09:04 +0900

    정답 코드

--- 2주차/BOJ 2636 치즈/이용혁.py
+++ 2주차/BOJ 2636 치즈/이용혁.py
@@ -1,55 +1,16 @@
-# Y, X = 13, 12
-#
-# cheese = [
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-#     [0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0],
-#     [0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-#     [0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0],
-#     [0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0],
-#     [0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0],
-#     [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-#     [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-#     [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-#     [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-# ]
-
-# Y, X = 5, 5
-# cheese =\
-# [[0, 0, 0, 0, 0],
-# [0, 0, 0, 0, 0],
-# [0, 0, 1, 0, 0],
-# [0, 0, 0, 0, 0],
-# [0, 0, 0, 0, 0]]
-
-
-Y, X = 10, 12
-cheese = \
-[[0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0, 0],
-[0, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1, 0],
-[0, 1, 1 ,0 ,0 ,1 ,0 ,0 ,0 ,1 ,1, 0],
-[0, 1, 0 ,1 ,0 ,1 ,0 ,1 ,1 ,0 ,1, 0],
-[0, 1, 0 ,1 ,0 ,1 ,0 ,0 ,0 ,0 ,0, 0],
-[0, 1, 0 ,1 ,0 ,0 ,1 ,1 ,1 ,0 ,1, 0],
-[0, 1, 0 ,1 ,1 ,1 ,1 ,1 ,1 ,0 ,1, 0],
-[0, 1, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1, 0],
-[0, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1, 0],
-[0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0, 0]]
-
-for c in cheese:
-    print(c)
-
-
 from collections import deque
+
+Y, X = map(int, input().split())
+cheese = [list(map(int, input().split())) for _ in range(Y)]
 
 queue = deque()
 
 dy = [1, 0, -1, 0]
 dx = [0, 1, 0, -1]
 
-y,x = 0,0
+y, x = 0, 0
+
+
 def BFS(cheese):
     queue.append((y, x))
     airs = [[0] * X for _ in range(Y)]
@@ -60,38 +21,40 @@ def BFS(cheese):
         for i in range(4):
             ny = here[0] + dy[i]
             nx = here[1] + dx[i]
-            if (nx < 0) or (nx > X - 1) or (ny > Y - 1) or (ny < 0) or (cheese[ny][nx] == 1) or visited[ny][nx]:  # 말 안되는 조건들 빼주기 # 최소거리 구하려면 돌아가면 안됨. # 0인 곳은 방문할 필요도 없음
+            if (nx < 0) or (nx > X - 1) or (ny > Y - 1) or (ny < 0) or (
+                    cheese[ny][nx] == 1) or visited[ny][
+                nx]:  # 말 안되는 조건들 빼주기 # 최소거리 구하려면 돌아가면 안됨. # 0인 곳은 방문할 필요도 없음
                 continue
-            #print("going :", (ny,nx))
             if cheese[ny][nx] == 0 and not visited[ny][nx]:
                 airs[ny][nx] = "a"  # 공기라고 표시
-                queue.append((ny,nx)) # 큐에 넣어두기
+                queue.append((ny, nx))  # 큐에 넣어두기
                 visited[ny][nx] = 1
     return airs
+
+
 loop = 0
 melted = []
+
 flag = True
 while flag:
     airs = BFS(cheese)
     s = set()
     visited = [[0] * X for _ in range(Y)]
-    for j in range(Y-1):
-        for i in range(X-1):
-            if cheese[j][i]==1:
+    for j in range(Y - 1):
+        for i in range(X - 1):
+            if cheese[j][i] == 1:
                 for k in range(4):
                     ny = j + dy[k]
                     nx = i + dx[k]
                     if airs[ny][nx] == 'a' and not visited[ny][nx]:
-                        s.add((j,i))
+                        s.add((j, i))
     melted.append(len(s))
     flag = len(s)
-    # print(sorted(s))
-    # print("previsous colelction is: ", len(s))
+
     # 녹이기 변환
     for t in s:
         cheese[t[0]][t[1]] = 0
-    # for c in cheese:
-    #     print(c)
+
     loop += 1
-print(loop-1)
+print(loop - 1)
 print(melted[-2])
