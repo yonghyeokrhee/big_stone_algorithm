055b7069aadfdbf1b94590d0d10d65d84549aea7
kanto <joshuaisland@gmail.com>
2023-08-04 17:23:28 +0900

    정답 코드

--- 3주차/BOJ 12869 뮤탈리스크/이용혁.py
+++ 3주차/BOJ 12869 뮤탈리스크/이용혁.py
@@ -1,12 +1,17 @@
 from collections import deque
 from itertools import permutations
+
 N = int(input())
-arr = list(map(int, input().split()))
-attack = [9,3,1]
-depth = 0
+inp = list(map(int, input().split()))
+arr = [0, 0, 0]
+for i in range(N):
+    arr[i] = inp[i]
+
+v = [[[0] * 64 for _ in range(64)] for _ in range(64)]
+attack = [9, 3, 1]
+
 
 def BFS(arr):
-    global depth
     q = deque()
     q.append(arr)
     while q:
@@ -14,10 +19,13 @@ def BFS(arr):
         for _ in range(l):
             this = q.popleft()
             if sum(this) == 0:
-                return depth
+                return v[this[0]][this[1]][this[2]]
             for p in permutations(attack[:N]):
-                q.append([max(scv-a,0) for scv, a in zip(this,p)])
+                hp = [max(scv - a, 0) for scv, a in zip(this, p)] + [0] * (3 - len(p))
+                if not v[hp[0]][hp[1]][hp[2]]:
+                    v[hp[0]][hp[1]][hp[2]] = v[this[0]][this[1]][this[2]] + 1
+                    q.append(hp)
 
-        depth +=1
 
-print(BFS(arr))
+v[arr[0]][arr[1]][arr[2]] += 1
+print(BFS(arr) - 1)
