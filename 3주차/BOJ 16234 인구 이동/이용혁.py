c5674235f17ef20c3e360793a5b27fc1b3ffb5e0
kanto <joshuaisland@gmail.com>
2023-08-06 07:42:29 +0900

    정답 코드

--- 3주차/BOJ 16234 인구 이동/이용혁.py
+++ 3주차/BOJ 16234 인구 이동/이용혁.py
@@ -1,10 +1,12 @@
-#시간 초과 문제 아직 해결 못함.
-
+# visited 배열 하나만 쓰니깐 성공
 from collections import deque
-N,L,R = map(int,input().split())
 
-arr = [list(map(int,input().split())) for _ in range(N)]
-def check(ny,nx,y,x):
+N, L, R = map(int, input().split())
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+
+def check(ny, nx, y, x):
     """
     this 와 there 의 차이가 L 이상 R 이하인가
     :return: boolean 타입으로 반환한다.
@@ -16,40 +18,38 @@ def check(ny,nx,y,x):
         return False
 
 
-def BFS(x,y)-> None:
+def BFS(x, y) -> None:
     """
     하나로 이어진 부분을 찾는다.
     :return:
     """
-    q=deque()
-    q.append((x,y))
-    c = [[0] * N for _ in range(N)]
+    q = deque()
+    q.append((x, y))
 
     v[y][x] = 1
-    c[y][x] = 1
 
-    dy = [-1,1,0,0]
-    dx = [0,0,-1,1]
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
     tot = arr[y][x]
     cnt = 1
     while q:
         x, y = q.popleft()
-        united.append([x,y]) # 연합국들을 여기에 저장해둔다. for 문 최소화하기 위해서.
+        united.append([x, y])  # 연합국들을 여기에 저장해둔다. for 문 최소화하기 위해서.
         # 연합국들은 다음 round에서는 조사할 필요가 없다. 연합국들은 주변국들에 의해서만 병합될 뿐이기 때문이다.
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
-            if ny< N and nx < N and nx >= 0 and ny >= 0 and not c[ny][nx] and check(ny,nx,y,x):
+            if ny < N and nx < N and nx >= 0 and ny >= 0 and not v[ny][
+                nx] and check(ny, nx, y, x):
                 v[ny][nx] = 1
-                c[ny][nx] = 1
-                q.append((nx,ny))
+                q.append((nx, ny))
                 tot += arr[ny][nx]
                 cnt += 1
 
     flag = 0
     if cnt == 1:
-        united.pop() # 하나 짜리 였다면 그냥 없애버리는게 맞다. 왜냐면 united는 전체 BFS에 대한 array이기 때문이다.
-    elif cnt > 1: # 연합국이 두개 이상이라면
+        united.pop()  # 하나 짜리 였다면 그냥 없애버리는게 맞다. 왜냐면 united는 전체 BFS에 대한 array이기 때문이다.
+    elif cnt > 1:  # 연합국이 두개 이상이라면
         flag = 1
         avg = int(tot / cnt)
         for _ in range(cnt):
@@ -57,25 +57,31 @@ def BFS(x,y)-> None:
 
     return flag
 
+
 flag = 1
-ans  = 0
+ans = 0
+
+united_check = list()
 
 while flag:
+
+    united = []
+    mig = []
 
     ret = 0
     v = [[0] * N for _ in range(N)]
 
-    imigration = [[0] * N for _ in range(N)]
     # 연합국 조사하기 -> 조사결과 좌표와 인구 저장해두자 (for 하나 더 줄일 수 있음)
-    united = []
-    mig = []
+
     for j in range(N):
         for i in range(N):
-            if not v[j][i]:
-                ret = max(BFS(i,j),ret)
+            if not v[j][i] and (i, j) not in united_check:
+                ret = max(BFS(i, j), ret)
 
     # 이민 하기
-    for u,m in list(zip(united,mig)): #반드시 연합국이 있는 나라들에 대해서만 연산해야 한다. united와 mig의 길이가 같아야 한다.
+    united_check = united.copy()
+    for u, m in list(zip(united,
+                         mig)):  # 반드시 연합국이 있는 나라들에 대해서만 연산해야 한다. united와 mig의 길이가 같아야 한다.
         i, j = u
         arr[j][i] = m
 
