8552d89fe575b22f82b4e2d23bfba495f7a4822d
kanto <joshuaisland@gmail.com>
2023-08-12 16:16:18 +0900

    정답 코드

--- 4주차/BOJ 5430 AC/이용혁.py
+++ 4주차/BOJ 5430 AC/이용혁.py
@@ -1,15 +1,19 @@
 from collections import deque
-
+# reverse는 O(n) 이라서 reverse 함수를 쓰면 안됨.
 T = int(input())
 
 def R(arr:deque)->deque:
-    arr.reverse()
+    global rev
+    rev = ~rev
 
     #return arr
 
 def D(arr:deque)->deque:
-    arr.popleft()
-    #return arr
+    global rev
+    if rev:
+        arr.pop()
+    else:
+        arr.popleft()
 
 for i in range(T):
     try:
@@ -17,8 +21,12 @@ for i in range(T):
         N = int(input())
         arr = eval(input())
         arr = deque(arr)
+        rev = err = False
         for f in F:
             eval(f)(arr)
-        print("[" + ",".join(map(str,arr)) + "]")
+        if rev:
+            print("[" + ",".join(map(str, reversed(arr))) + "]")
+        else:
+            print("[" + ",".join(map(str,arr)) + "]")
     except:
         print("error")
