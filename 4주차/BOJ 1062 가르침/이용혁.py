204404057dc0e3419f22d95e843522713e5d16a2
kanto <joshuaisland@gmail.com>
2023-08-17 14:20:17 +0900

    디버깅 코드 : 시간 초과

---
+++ 4주차/BOJ 1062 가르침/이용혁.py
@@ -0,0 +1,53 @@
+n,k = map(int, input().split())
+tcs = [input() for i in range(n)]
+
+arr = [0] * 26
+
+for s in ['a','n','t','i','c']:
+    arr[ord(s)-97]=1
+
+#추가로 조합 경우의 수를 arr에 넣는 코드가 필요하다.
+
+alpha = [i-97 for i in range(97,97+26) if chr(i) not in ['a','n','t','i','c']]
+
+result = []
+def dfs(indx, path,k):
+    """
+    기존에 있는 5개의 alphabet 을 제외하고 나머지 21개의 alphabet 중에서 3개에 해당하는 모든 경우를 추출하는 함수
+    :param indx:
+    :param path:
+    :param k: 조합을 구성하는 요소의 개수
+    :return: array of all possible combinations
+    """
+    if len(path) == k:
+        result.append(path[:])
+        return
+
+# alpha[i] 여기를 indx로 잘 못 표기해서 고생했음. 내일 기록해두자.
+# 그리고 index가 무한정 진행되지 않으려면 장치 하나 더 필요한듯.
+    for i in range(indx, len(alpha)):
+        path += [alpha[i]]
+        dfs(i+1, path,k)
+        path.pop()
+    return result
+
+mx = 0
+
+comb = dfs(0, [],k-5)
+
+# 모든 test case에 대하여 시도할 수 있어야 한다.
+
+for c in comb:
+    new_arr = arr.copy()
+    success = 0
+    # 조합의 요소를 기존의 arr에 더하여 new_arr에 채워넣는다.
+    for i in c:
+        new_arr[i] = 1
+    for tc in tcs:
+        if all([new_arr[ord(t)-97] for t in tc]):
+            # print(f"{tc} is success")
+            success += 1 #성공횟수 cnt
+    if success > mx:
+        mx = success # 최대 읽을 수 있는 단어 개수의 최대값을 udpate
+
+print(mx)
