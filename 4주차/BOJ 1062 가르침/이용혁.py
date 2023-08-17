fc6d124c8df328a06a7948ff20d560d90ade360f
kanto <joshuaisland@gmail.com>
2023-08-17 18:52:21 +0900

    정답 코드(비트마스킹)

--- 4주차/BOJ 1062 가르침/이용혁.py
+++ 4주차/BOJ 1062 가르침/이용혁.py
@@ -1,59 +1,32 @@
+## 비트마스킹 풀이
+
 n,k = map(int, input().split())
 tcs = [input() for i in range(n)]
 
-arr = [0] * 26
+words = [0] * 51
 
-for s in ['a','n','t','i','c']:
-    arr[ord(s)-97]=1
+for i in range(n):
+    for char in tcs[i]:
+        words[i] |= (1 << ord(char)-97) # 비트를 하나씩 계속 킨다. 안켜져 있으면 키고 켜져 있으면 내버려둔다. 비트를 키는 방식으로 켜야하는 알파벳을 업데이트를 한다.
+        # 모든 알파벳의 비트가 다 켜진 상황은 2^26 -1 일 것이고 a 만켜져 잇으면 1, b만 켜져 있으면 2, ab 켜져있으면 3 이런식이다.
+# 모든 테스트 케이스에 대한 비트 기록을 words 에 저장한 뒤에 조합을 뽑아서 검사한다.
 
-#추가로 조합 경우의 수를 arr에 넣는 코드가 필요하다.
-
-    def dfs(indx, path, k):
-        """
-        기존에 있는 5개의 alphabet 을 제외하고 나머지 21개의 alphabet 중에서 3개에 해당하는 모든 경우를 추출하는 함수
-        :param indx:
-        :param path:
-        :param k: 조합을 구성하는 요소의 개수
-        :return: array of all possible combinations
-        """
-        if len(path) == k:
-            result.append(path[:])
-            return
-
-        # alpha[i] 여기를 indx로 잘 못 표기해서 고생했음. 내일 기록해두자.
-        # 그리고 index가 무한정 진행되지 않으려면 장치 하나 더 필요한듯.
-        for i in range(indx, len(alpha)):
-            path += [alpha[i]]
-            dfs(i + 1, path, k)
-            path.pop()
-        return result
-
-if k <5:
-    print(0)
-elif k == 5:
+def count(mask): # 마스킹 되어있는 비트를 출력한다.
     cnt = 0
-    for tc in tcs:
-        if all([arr[ord(t) - 97] for t in tc]):
-            cnt += 1
-    print(cnt)
-else:
-    alpha = [i-97 for i in range(97,97+26) if chr(i) not in ['a','n','t','i','c']]
-    result = []
-    mx = 0
-    comb = dfs(0, [], k - 5)
+    for tc in words:
+        if tc and (tc & mask) == tc:
+            cnt +=1
+    return cnt
 
-    for c in comb:
-        new_arr = arr.copy()
-        success = 0
-        # 조합의 요소를 기존의 arr에 더하여 new_arr에 채워넣는다.
-        for i in c:
-            new_arr[i] = 1
-        for tc in tcs:
-            if len(tc) == 8:
-                success +=1
-            elif all([new_arr[ord(t)-97] for t in tc[4:-4]]):
-                success += 1 #성공횟수 cnt
-        if success > mx:
-            mx = success # 최대 읽을 수 있는 단어 개수의 최대값을 udpate
+def go(index, k, mask):
+    if (k<0):
+        return 0
+    if index == 26:
+        return count(mask)
+    ret = go(index+1, k-1, mask|1<<index)
+    if (index != (ord('a') - 97) and index != (ord('n') - 97) and index != (ord('t') - 97) and index != (ord('i') - 97) \
+and index != (ord('c') - 97)):
+        ret = max(ret, go(index+1, k ,mask))
+    return ret
 
-    print(mx)
+print(go(0,k,0))
