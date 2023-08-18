770ac3e663bb159d957ef215706bcc9ea20858cf
kanto <joshuaisland@gmail.com>
2023-08-18 15:25:11 +0900

    정답 코드

---
+++ 5주차/BOJ 1931 회의실 배정/이용혁.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+arr = sorted(arr, key = lambda x: (x[1],x[0]))
+
+line = cnt = 0
+for a in arr:
+    if line <= a[1] and line <= a[0]:
+        line = a[1]
+        cnt += 1
+        print(a)
+    else:
+        if line > a[0]:
+            continue
+
+print(cnt)
