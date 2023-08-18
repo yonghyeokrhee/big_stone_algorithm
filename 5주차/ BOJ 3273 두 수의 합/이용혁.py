dff1c1cd86034b60f5201dde47cf2935b708c5db
kanto <joshuaisland@gmail.com>
2023-08-18 17:03:16 +0900

    정답 코드

--- 5주차/ BOJ 3273 두 수의 합/이용혁.py
+++ 5주차/ BOJ 3273 두 수의 합/이용혁.py
@@ -4,11 +4,11 @@ k = int(input())
 
 mem = [0] * 1000001 # 1백만 1 이하로만 기록해도 된다. 그 이상은 기록 할 필요가 없음.
 
-## 200만 보다 큰 수에 대해 x 값이 제시되었을 때 예외 처리 해보기.
-
 cnt = 0
 for i in arr:
-    if mem[k-i]:
+    if k-i >= 1000001:
+        continue
+    elif mem[k-i]:
         cnt += 1
     else:
         mem[i] = 1
