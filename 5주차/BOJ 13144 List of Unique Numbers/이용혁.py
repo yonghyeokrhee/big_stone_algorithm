0a1bf7fdf3a75995f77331f457511a88fa83ef9d
kanto <joshuaisland@gmail.com>
2023-08-18 20:00:27 +0900

    정답 코드(답참조)

--- 5주차/BOJ 13144 List of Unique Numbers/이용혁.py
+++ 5주차/BOJ 13144 List of Unique Numbers/이용혁.py
@@ -1,27 +1,19 @@
 n = int(input())
 arr = [*map(int,input().split())]
 
-mem = [0] * 100001
 # 가장 멀리갈 수 있는 unique 한 배열의 끝을 찾아본다.
+mem = [0] * 100001
+e = 0
+s = 0
+ret = 0
+while e < n:
+    if not mem[arr[e]]:
+        mem[arr[e]] +=1
+        e +=1
+    else:
+        ret += (e-s)
+        mem[arr[s]] -= 1
+        s+=1
 
-# line sweeping style 문제
-cnt = 0
-flag = False
-for j in range(n):
-    for i in range(j,n):
-        if not mem[arr[i]]:
-            mem[arr[i]] = 1 # 없으면 채운다.
-            if (i-j) + 1 == n-j:
-                flag = True
-                break
-        else:
-            cnt += (i-j) # 겹치지 않고 이동한 최장 거리를 기록한다.
-            mem[arr[j]] = 0  # 10만 이하에 해당하는 수를 저장하기. 단, break가 되면 갱신한다.
-            break
-    if flag:
-        break
-k = n-j
-cnt += (k * (k+1) ) // 2
-
-print(cnt)
-
+ret += ((e-s) * (e-s+1)) // 2
+print(ret)
