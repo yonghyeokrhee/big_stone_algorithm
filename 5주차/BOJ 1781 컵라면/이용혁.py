0097517a1a97859815faccfcca96a0997845da59
kanto <joshuaisland@gmail.com>
2023-08-19 19:44:30 +0900

    정답 코드 (간결하게 정리)

---
+++ 5주차/BOJ 1781 컵라면/이용혁.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+import sys
+input = sys.stdin.readline
+from queue import PriorityQueue
+n = int(input())
+
+mydict = defaultdict(list) # key를 날짜, value를 강연료로 하는 dictionary
+q = PriorityQueue()
+
+for _ in range(n):
+    key,v= map(int, input().split())
+    mydict[key].append(v)
+
+keys = sorted(list(mydict.keys()))
+for k in keys:
+    lecs = mydict[k]
+    i = 0
+    while True:
+        q.put(lecs[i])
+        if q.qsize() > k:
+            q.get()
+        i += 1
+        if i == len(lecs):
+            break
+
+ret = 0
+for i in range(q.qsize()):
+    ret += q.get() # 작은 순서대로 꺼내는 HQ
+print(ret)
