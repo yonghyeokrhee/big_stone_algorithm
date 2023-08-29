a73429b2f2859ac392c5327c69c498dc489a712e
kanto <joshuaisland@gmail.com>
2023-08-29 16:57:24 +0900

    정답 코드

--- 5주차/BOJ 2170 선 긋기/이용혁.py
+++ 5주차/BOJ 2170 선 긋기/이용혁.py
@@ -3,10 +3,10 @@ input = sys.stdin.readline
 K = int(input())
 arr = [[*map(int, input().split())] for _ in range(K)]
 ret = []
-s = -1000000000
-e = -1000000000
 
 arr = sorted(arr,key = lambda x: (x[0])) # 정렬
+s = arr[0][0]
+e = arr[0][1]
 
 ret = []
 for i in arr:
@@ -22,10 +22,6 @@ for i in arr:
 ret.append((s,e)) # 마지막 결과 저장
 answer = 0
 for i in range(len(ret)):
-    print(ret[i])
-    if ret[i][0] == -1000000000 and  ret[i][1] == -1000000000:
-        continue
-    else:
-        line = ret[i]
-        answer+=abs(line[1] - line[0])
+    line = ret[i]
+    answer+=abs(line[1] - line[0])
 print(answer)
