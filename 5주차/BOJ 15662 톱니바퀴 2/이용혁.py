b06e1ed4efa9ee515569325ef74225e1fc25e71b
kanto <joshuaisland@gmail.com>
2023-08-29 15:49:03 +0900

    오답 코드

--- 5주차/BOJ 15662 톱니바퀴 2/이용혁.py
+++ 5주차/BOJ 15662 톱니바퀴 2/이용혁.py
@@ -4,29 +4,47 @@ arr = [deque([*map(int, list(input()))]) for _ in range(K)]
 n = int(input())
 rots = [[*map(int, input().split())] for _ in range(n)]
 
-# for i in arr:
-#     print(i)
-#
+for i in arr:
+    print(i)
+
 # for i in rots:
 #     print(i)
 
+def go(elem,r):
+    if r: # 오른쪽 확인
+        # print(elem)
+        if elem+1 >= K or arr[elem][2] == arr[elem + 1][-2]:
+            return False
+        else:
+            go(elem+1,r)
+            arr[elem+1].rotate(-d)  # 반대쪽으로 회전 시킨다
+    else: # 왼쪽 확인
+        if elem - 2 <= 0 or arr[elem][-2] == arr[elem -1][2]:
+            return False
+        else:
+            go(elem -1,r)
+            arr[elem -1].rotate(-d)  # 반대쪽으로 회전 시킨다
+
+
+
+def do_rotate(elem,d)->bool:
+    """조건을 확인하고 돌리는 함수"""
+    go(elem-1, True) #오른쪽
+    go(elem-1, False) #왼쪽
+    arr[elem - 1].rotate(d) # 자신을 회전한다.
+
 # 왼쪽 돌리기 구현
-
+flag=0
 for elem, d in rots:
-    l,r = arr[elem-1][-2], arr[elem-1][2]
-    # 3번의 오른쪽 왼쪽에 대한 돌리기를 구현한다.
-    # 왼쪽
-    if elem-2 <= 0 or l == arr[elem-2][2]: # 극이 같으면 (혹은 왼쪽 끝)
-        if elem >= K or r == arr[elem][-2]: #극이 같으면 (혹은 오른쪽 끝)
-            continue # 양쪽 모두 그대로
-        else: # 오른쪽에 돌릴 것이 있다면
-            arr[elem].rotate(-d)  # 반대쪽으로 회전 시킨다
-    else:
-        arr[elem-2].rotate(-d) # 반대쪽으로 회전 시킨다
+    flag += 1
+    print(f"this time {flag} is executed")
+    do_rotate(elem,d)
+    print("========rotation ends =========")
+    for i in arr:
+        print(i)
 
-    arr[elem-1].rotate(d) # elem 번째 톱니를 direction으로 회전한다. 한 번.
+print("answer is ....")
 
-    # print("========rotation ends =========")
 answer = 0
 for i in arr:
     if i[0] == 1:
