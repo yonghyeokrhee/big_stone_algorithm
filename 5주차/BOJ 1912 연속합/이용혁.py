79d150e6522d38d6d889a4007e8e5e7849f76260
kanto <joshuaisland@gmail.com>
2023-09-09 17:02:38 +0900

    create

--- 5주차/BOJ 1912 연속합/이용혁.py
+++ 5주차/BOJ 1912 연속합/이용혁.py
@@ -1,16 +1,14 @@
 n = int(input())
 arr = [*map(int, input().split())]
 answer = []*n
-mx = -9876754321
+mx = arr[0]
 for i in range(1,n):
-    if arr[i-1] > mx:
-        mx = arr[i-1]
+
     if arr[i-1] < 0:
-        continue
+        pass
     else:
         arr[i] = arr[i] + arr[i-1]
-
-if arr[n-1] > mx:
-    mx = arr[n-1]
+    if arr[i] > mx:
+        mx = arr[i]
 
 print(mx)
