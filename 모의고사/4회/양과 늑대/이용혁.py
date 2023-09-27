8714b97825f7cee863351ae352c69adb865904f9
yonghyeokrhee <joshuaisland@gmail.com>
2023-09-28 00:18:53 +0900

    오답 코드

---
+++ 모의고사/4회/양과 늑대/이용혁.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+
+cnt_lamb = 0
+cnt_wolf = 0
+visit = [0] * 18
+
+def solution(info, edges):
+    d = defaultdict(list)
+    for e in edges:
+        d[e[0]].append(e[1])
+
+    def dfs(node):
+        global cnt_lamb, cnt_wolf
+        if info[node] and not visit[node]:
+            cnt_wolf += 1
+        elif not info[node] and not visit[node]:
+            cnt_lamb += 1
+            status = True
+        visit[node] = True
+        
+        if not d[node]:
+            return status
+        for there in sorted(d[node],
+                            key=lambda x: info[x]):  # 0부터 정렬해서 양이 있는 것부터..
+
+            if cnt_wolf + info[there] >= cnt_lamb:
+                continue
+            status = dfs(there)
+            if info[there] and not status:
+                cnt_wolf -= 1
+
+
+    flag = True
+    while flag:
+        prev = cnt_lamb
+        dfs(0)
+        post = cnt_lamb
+        flag = max(post - prev, 0)
+
+    return cnt_lamb
