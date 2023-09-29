799ed5a8969e6aef48e175e3b9dbc0eeee7cbcb5
yonghyeokrhee <joshuaisland@gmail.com>
2023-09-29 23:41:10 +0900

    정답 코드

--- 모의고사/2회/표현 가능한 이진 트리/이용혁.py
+++ 모의고사/2회/표현 가능한 이진 트리/이용혁.py
@@ -1,16 +1,15 @@
-
-def binary_search(start, end, bi,flag)->bool:
-    # flag가 있는 상태에서 array 안에 1이 존재하는 경우
-    if flag and '1' in bi:
-        return False
-    # 끝까지 도달  한 경우
+import sys
+sys.setrecursionlimit(1000000)
+def binary_search(start, end, bi)->bool:
+    mid = (start + end) //2
+    # flag가 있는 상태에서 root node 좌우로 1이 있는 경우
     if start == end-1:
         return True
-    mid = (start + end) //2
-    if not int(bi[mid]):
-        flag = True
+    if not bi[mid] and (bi[(start+mid)//2]==1 or bi[(mid+1+end)//2]==1):
+        return False
+    # 끝까지 도달  한 경우 node val은 0 또는 1이며 False를 반환한 적이 없으므로 문제가 없다.
 
-    return binary_search(start, mid, bi,flag) and binary_search(mid+1,end, bi,flag)
+    return binary_search(start, mid, bi) and binary_search(mid+1,end, bi)
 
 
 def solution(numbers):
@@ -18,12 +17,11 @@ def solution(numbers):
     for num in numbers:
         bi = bin(num).split('b')[-1]
         n = 0
-        while len(bi) > 2**n:
+        while len(bi) >= 2**n:
             n+=1
         # numbers to add prefix
         for _ in range(2**n-1 - len(bi)):
             bi = '0'+bi
-        answer.append(binary_search(0,2**n-1, bi, False))
+        bi = [*map(int,list(bi))]
+        answer.append(binary_search(0,2**n-1, bi))
     return [*map(int,answer)]
-
-print(solution([5]))
