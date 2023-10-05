c56f13bb1dfbe20d254481f08e758f5acea7383a
kanto <joshuaisland@gmail.com>
2023-10-05 13:55:53 +0900

    정답 코드

--- 모의고사/6회/후보키/이용혁.py
+++ 모의고사/6회/후보키/이용혁.py
@@ -1,36 +1,73 @@
 from collections import Counter, defaultdict
 
-def check_unique(relation,candidate):
-    if len(candidate)==0:
-        return True
+
+def minimality(result):
+    answer = []
+
+    for x in result:
+
+        flag = False
+
+        for y in result:
+            if x == y:
+                continue
+            else:
+                if set(y).issubset(set(x)):
+                    flag = True
+                    break
+        if flag:
+            continue
+        answer.append(x)
+    return answer
+
+
+def check_unique(relation, candidate):
+    if len(candidate) == 0:
+        return False
     keys = defaultdict(int)
     for row in relation:
         key = ''.join([row[cand] for cand in candidate])
-        keys[key] +=1
-        if keys[key] >=2:
+        keys[key] += 1
+        if keys[key] >= 2:
             return False
     return True
 
-def dfs(keys:list, candidates:list, relation,  k:int):
-    """
-    :param keys : 후보키를 만들 수 있는 bucket
-    :param candidates: 후보키 리스트
-    :param k: k는 후보키 조합의 개수
-    :return:
-    """
-    if k == 0:
-        return # w종료
-    if check_unique(relation, candidates):
-        answer.append(candidates[:])
-        return # dfs 종료
-    for i in keys:
-        candidate = keys.pop(i)
-        dfs(keys, candidates.append(candidate),relation , k-1)
-        candidates.pop()
+
+result = []
+
+
+def mycombinations(arr, path, result, index, relation):
+    if len(path) > 0 and check_unique(relation, path[:]):
+        result.append(path[:])
+        path[:].pop()
+        return
+    for i in range(index, len(arr)):
+        path.append(arr[i])
+        mycombinations(arr, path, result, i + 1, relation)
+        path.pop()
+
+
 def solution(relation):
     cands = [i for i in range(len(relation[0]))]
-    dfs(cands, [], relation, len(relation[0]))
-    return answer
-global answer
-answer = []
+    mycombinations(cands, [], result, 0, relation)
+    print(result)
+    answer = minimality(result)
+    print(answer)
+    return len(answer)
+
 print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
+
+
+
+
+# 조합 만들기 (반드시 외워두자...)
+# result = []
+# def combinations(arr, path, result,index):
+#     result.append(path[:])
+#     for i in range(index, len(arr)):
+#         path.append(arr[i])
+#         combinations(arr, path, result,i+1)
+#         path.pop()
+#
+# combinations([1,2,3],[],result,0)
+# print(result)
