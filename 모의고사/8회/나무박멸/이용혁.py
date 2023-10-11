cec67bfc3916bcf15610c4796e1bac149a7cc9a8
kanto <joshuaisland@gmail.com>
2023-10-11 15:35:49 +0900

    디버그 필요..

--- 모의고사/8회/나무박멸/이용혁.py
+++ 모의고사/8회/나무박멸/이용혁.py
@@ -7,41 +7,39 @@ dy = [0,1,0,-1]
 dx = [1,0,-1,0]
 answer = 0
 # 성장
+
+def count_something(x, y, some):
+    some = 0
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if ny < 0 or nx < 0 or ny > N - 1 or nx > N - 1 or ptime[ny][nx] or arr[ny][nx] == -1:
+            continue
+        if some == 'free':
+            if arr[ny][nx] == 0:
+                some += 1
+        elif some == 'tree':
+            if arr[ny][nx] > 0:
+                some +=1
+    return some
+
+
 def grow(arr):
-    def count_trees(x,y)->int:
-        trees = 0
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if ny < 0 or nx < 0 or ny > N - 1 or nx > N - 1 or ptime[ny][nx] or arr[ny][nx] <= 0:
-                continue
-            trees +=1
-        return trees
 
     for y in range(N):
         for x in range(N):
             if ptime[y][x] or arr[y][x] <= 0:
                 continue
-            arr[y][x] += count_trees(x,y) # todo: 주변의 나무 개수를 count 하여 더해준다.
+            arr[y][x] += count_something(x,y,'tree') # todo: 주변의 나무 개수를 count 하여 더해준다.
     return arr
 
-# 번식 - 빈칸이 있는 경우에 그 수만큼 나누어주고 번식한다. (나무는 제초제 위치를 알고 있음)
 def expansion(arr):
-    def count_frees(x,y):
-        frees = 0
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if ny < 0 or nx < 0 or ny > N - 1 or nx > N - 1 or ptime[ny][nx] or arr[ny][nx] > 0 or arr[ny][nx] == -1: # P는 제초제 유효기간과 관련이 있다.
-                continue
-            frees += 1
-        return frees
     narr = [[0] * N for _ in range(N)]
     for y in range(N):
         for x in range(N):
             if ptime[y][x]:
                 continue
-            free = count_frees(x,y)
+            free = count_something(x,y,'free')
             if not free:
                 continue
             rep = arr[y][x] // free #todo: 실제 주변에 번식 가능여부를 cnt 한다.
@@ -62,13 +60,11 @@ def expansion(arr):
 
     return narr
 
-# 박멸 - 대각선으로 박멸한다.
 dy2 = [1,-1,-1,1] # 대각선 움직임
 dx2 = [1,-1,1,-1]
 
 
 def reset_pest():
-    # 제초제가 남아있는지 확인하고 제초제 시간이 경과했다면 0으로 reset 해준다.
     for y in range(N):
         for x in range(N):
             if ptime[y][x]:
@@ -97,9 +93,7 @@ def check(arr,k)->list:
                 mx = cut
                 mx_x = x
                 mx_y = y
-    # 가장 많은 나무를 제초할 수 있는 위치를 찾는 함수
-    # print(f"\n 가장 많은 제초 나무 숫자 : {mx}")
-    # print(f"\n 가장 많은 제초 나무 위치 : x:{d[mx][0][0]}, y:{d[mx][0][1]}")
+
     return mx, mx_x, mx_y
 
 def eliminate(arr,k):
@@ -108,7 +102,6 @@ def eliminate(arr,k):
     count, x,y = check(arr,k) # 제초제를 뿌릴 위치를 찾는다.
     if not count:
         return arr # 종료
-    print(f"\n제초 나무 합계: ", count)
     answer += count
 
     ptime[y][x] = C
@@ -126,23 +119,10 @@ def eliminate(arr,k):
 
 
 for m in range(M):
-    print("\n Year:",m)
 
-    print("\n성장")
-    for ar in arr:
-        print(ar)
     arr = grow(arr) # 성장한다.
-
     arr = expansion(arr) # 번식한다.
     reset_pest() # pest 의 수명을 -1 감소시킨다.
-    print("\n번식")
-    for ar in arr:
-        print(ar)
     arr = eliminate(arr,K) # 제초제를 뿌린다.
-    print("\n제초체")
-    for ar in arr:
-        print(ar)
-    print("\n제초 유효기간")
-    for pt in ptime:
-        print(pt)
+
 print(answer)
