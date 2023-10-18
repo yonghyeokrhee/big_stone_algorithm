bcf8555124957c6535100e85bffc4d5eabf50965
kanto <joshuaisland@gmail.com>
2023-10-18 15:19:12 +0900

    보다 간략하게 BFS 먼저 짜기 위한 임시기록

--- 모의고사/1회/경주로 건설/이용혁.py
+++ 모의고사/1회/경주로 건설/이용혁.py
@@ -1,18 +1,17 @@
 from collections import deque
 def solution(board):
     n = len(board)
-    start = [0,0]
+    init = [[0,0,0,0],[0,0,0,1]] # 내려가는 방향과 오른쪽으로 가는 방향 두 가지를 초기값으로.
     end = [len(board)-1, len(board)-1]
-    # array는 4차원으로 관리한다. (x,y,코너 수, 진향방향(어디서 왔는가))
+    # array는 4차원으로 관리한다. (x,y,코너 수, 진행방향(어디서 왔는가)) <- 카카오 기출 풀이
     space = [[[[[0] for _ in range(n)] for _ in range(n)] for _ in range(n**n)] for _ in range(4)]
     answer = 0
     q = deque()
-    q.append(start)
+    q.extend(init)
     dy = [1,0,-1,0]
     dx = [0,1,0,-1]
-    direction = None
     while q:
-        y,x = q.popleft()
+        y, x, coners, direction = q.popleft()
         for i in []:
             # i = 0 down
             # i = 1 right
@@ -20,10 +19,14 @@ def solution(board):
             # i = 3 left
             ny = y + dy[i]
             nx = x + dx[i]
-            if board[ny][nx] == 0 and  0 <= ny < len(board) and 0<= nx < len(board):
-
-                s[ny][nx][]
-                s[ny][nx][][i]=100
+            if board[ny][nx] == 0 and  0 <= ny < len(board) and 0<= nx < len(board): # 진행한다.
+                # 직진
+                if i == direction:
+                    board[ny][nx][coners][i] =
+                # 좌회전
+                # 우회전
+                cost = dist*100
+                space[ny][nx][dist][i] = cost
 
     return answer
 
